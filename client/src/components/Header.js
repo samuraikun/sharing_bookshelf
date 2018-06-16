f9f31353e80a3e6daf318a09e163084dd28ed963
samuraikun <tazitaziawawa@gmail.com>
2018-06-16 17:31:59 +0900

    remove logo from Header Compoent

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from '../logo.svg';
 
 const Header = () => {
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Sharing BookShelf</h2>
+      <h3>Sharing BookShelf</h3>
     </header>
   );
 }
